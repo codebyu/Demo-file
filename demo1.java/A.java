e16fe068d15d2dbca51da517010e5c3f5840542b
codebyu <chttimtii@ gmail.com>
2024-07-10 01:02:29 +0700

    YES

---
+++ demo1.java/A.java
@@ -0,0 +1,13 @@
+class A
+
+    { public static void main (String[] args)     
+       String a = "Darshan";
+       String b = "AKKU";
+     System.out.println(a.length());
+     System.out.println(a.toUpperCase());
+       System.out.println(b.toLowerCase());
+
+        
+    }
+
+    }
---
+++ demo1.java/operators.java
@@ -0,0 +1,14 @@
+class operators {
+    public static void main(String[] args)
+   { int a = 2;
+    int b = 5;
+                        
+    System.out.println(a+b);
+    System.out.println(a*b);
+ //Now lets print the multiplication and division
+   System.out.println(a*b);
+    
+    System.out.println(a/b);
+    System.out.println("Ye hai Hmara Program");
+   }
+}
