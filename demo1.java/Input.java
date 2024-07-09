31880102d276069edaeb823bfec120cfc4fa8ac1
codebyu <chttimtii@ gmail.com>
2024-07-10 02:23:43 +0700

    Ok

---
+++ demo1.java/Input.java
@@ -0,0 +1,19 @@
+import java.util.Scanner;
+public class Input 
+{ public static void main (String args[]) 
+  { 
+    int a;
+   {  System.out.println("Lets Square the Number");
+    Scanner obj= new Scanner(System.in);
+     System.out.println("Enter the value bro");
+     a=obj.nextInt();
+    
+     
+     System.out.println(a*a);
+
+     System.out.println("Thank You");
+
+
+   }
+}
+}
